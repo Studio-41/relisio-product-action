23ae3bcdb7354514d7873cfda5aa14d9e9f81552
Marco Casula <marco.casula@gmail.com>
2022-03-03 22:09:10 +0100

    pass originalId as body payload
    OriginalId can be used to clone an existing product into your relisio
    account.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core'
-import {createHash} from 'crypto'
 import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
@@ -20,19 +19,9 @@ async function run(): Promise<void> {
       throw new Error('relisio-url is required')
     }
 
-    const now = new Date().getTime()
+    const originalId = core.getInput('product-template-id')
 
-    let productId = core.getInput('product-id')
-
-    if (!productId) {
-      core.debug('product-id is not set, a new product will be created')
-
-      productId = createHash('sha256')
-        .update(`${workspacePath}/products/${now}`)
-        .digest('hex')
-    }
-
-    const publicUrl = `https://${relisoUrl}/api/v1/workspaces/${workspacePath}/products/${productId}`
+    const publicUrl = `https://${relisoUrl}/api/v1/workspaces/${workspacePath}/products`
 
     const response = await fetch(publicUrl, {
       method: 'POST',
@@ -40,14 +29,19 @@ async function run(): Promise<void> {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${apiKey}`
       },
-      body: JSON.stringify({})
+      body: JSON.stringify({
+        originalId
+      })
     })
 
     if (!response.ok) {
       core.setFailed(`could not create product: ${response.statusText}`)
       return
     }
-    core.setOutput('product-id', productId)
+
+    const {_id = ''} = (await response.json()) as {_id: string}
+
+    core.setOutput('product-id', _id)
     core.setOutput('public-url', publicUrl)
   } catch (error) {
     core.debug(`Deployment Failed with Error: ${error}`)
